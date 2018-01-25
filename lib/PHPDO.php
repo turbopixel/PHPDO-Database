0c27a99dd3cbc4988d811b2c5ec4a6e37911390d
Nico <nico@nokes.de>
2018-01-25 10:18:09 +0100

    CHANGE: default utf8

--- lib/PHPDO.php
+++ lib/PHPDO.php
@@ -45,7 +45,8 @@ class PHPDO {
     $opt = [
       PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
       PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-      PDO::ATTR_EMULATE_PREPARES   => false
+      PDO::ATTR_EMULATE_PREPARES   => false,
+      PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES "utf8"'
     ];
 
     try {
