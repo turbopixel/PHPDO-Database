e7859ed834f57294c109dbbd32020acd075b1083
Nico <nico@nokes.de>
2018-01-25 10:00:30 +0100

    CHANGE: pdo options

--- lib/PHPDO.php
+++ lib/PHPDO.php
@@ -28,8 +28,14 @@ class PHPDO {
    */
   public function connect(string $host, string $database, string $user, string $password, int $port = 3306) {
 
+    $opt = [
+      PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      PDO::ATTR_EMULATE_PREPARES   => false
+    ];
+
     try {
-      $this->PDO = new PDO("mysql:host={$host};dbname={$database};port={$port}", $user, $password);
+      $this->PDO = new PDO("mysql:host={$host};dbname={$database};port={$port}", $user, $password, $opt);
     }
     catch (PDOException $e) {
       die($e->getMessage());
